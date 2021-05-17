de64b767d3a29cbf92a2ed796daea73faf100a19
TChamp2 <68830714+TChamp2@users.noreply.github.com>
2021-05-17 13:41:37 -0700

    Controller Script for the first boss
    This script controls the first boss

---
+++ Assets/Scripts/Boss1Controller.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Boss1Controller : MonoBehaviour
+{
+    private Rigidbody2D myRigidbody;            // Rigidbody of the enemy
+    [SerializeField] int health = 100;          // Health of the boss
+    [SerializeField]Text healthText;            // Text of the health
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody2D>();
+        healthText.text = "Boss Health: " + health.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(health <= 0)
+        {
+            Destroy(gameObject);
+        }
+
+        healthText.text = "Boss Health: " + health.ToString();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.name.Contains("Sword"))
+        {
+            //Debug.Log("sword hit");
+            health--;
+        }
+    }
+}
