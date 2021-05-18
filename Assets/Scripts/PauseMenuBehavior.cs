f68998d65b5f1cbfb8db784600adfd7b417fd98c
Jasmine H <thejasminehansen@gmail.com>
2021-05-18 14:04:03 -0500

    Added Basic Pause Menu
    Pause menu UI prefab added (includes a behavior script)
    Pause menu added to levels 1, 2 (requires setting the camera, having an EventSystem)

---
+++ Assets/Scripts/PauseMenuBehavior.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenuBehavior : MonoBehaviour
+{
+    public static bool gameIsPaused;
+
+    private GameObject pauseButton;
+    private GameObject pausePanel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // The PauseCanvas and it's children need to be active
+        // so that the GameObject.Find() function works
+        pauseButton = GameObject.Find("Pause Button");
+        pausePanel = GameObject.Find("Backdrop");
+
+        pauseButton.SetActive(true);
+        pausePanel.SetActive(false);
+    }
+    
+    // Pause Button
+    public void PauseGame()
+    {
+        Time.timeScale = 0;
+        gameIsPaused = true;
+
+        pauseButton.SetActive(false);
+        pausePanel.SetActive(true);
+    }
+
+    // Resume Button
+    public void ResumeGame()
+    {
+        Time.timeScale = 1;
+        gameIsPaused = false;
+
+        pauseButton.SetActive(true);
+        pausePanel.SetActive(false);
+    }
+}
