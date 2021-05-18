2dd700648a1c0ea1c14c736ec3782e5e5a6fb395
TChamp2 <68830714+TChamp2@users.noreply.github.com>
2021-05-18 11:35:33 -0700

    Boss stuff
    I did boss stuff

---
+++ Assets/Scripts/BossRotate.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossRotate : MonoBehaviour
+{
+    private Rigidbody2D myRigidbody;            // Rigidbody of the enemy
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        myRigidbody.rotation += 0.2f;   
+    }
+}
