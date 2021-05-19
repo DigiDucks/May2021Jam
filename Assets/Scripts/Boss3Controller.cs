d8d0935d09c982d41a635f464e6480ab4ab26ff7
TChamp2 <68830714+TChamp2@users.noreply.github.com>
2021-05-19 14:57:38 -0700

    Boss 3 progress
    Title says it

--- Assets/Scripts/Boss3Controller.cs
+++ Assets/Scripts/Boss3Controller.cs
@@ -8,6 +8,10 @@ public class Boss3Controller : MonoBehaviour
     private Rigidbody2D myRigidbody;// variable handles the rigid body of the enemy
     [SerializeField] int health = 100;          // Health of the boss
     [SerializeField] Text healthText;            // Text of the health
+    [SerializeField] GameObject projectilePrefab; // prefab of the projectile the enemy shoots
+    private float shotTimer = 0f;// start of timer before shooting the projectile
+    [SerializeField] float shotTimerMax = 3f;// Max time before shooting the player
+    [SerializeField] float projectileSpeed = 5f;//speed of the projectile fired
 
     // Start is called before the first frame update
     void Start()
@@ -23,10 +27,37 @@ public class Boss3Controller : MonoBehaviour
         {
             Destroy(gameObject);
         }
-        //if (health <= 50)
-        //{
-        //    myRigidbody.rotation += 0.3f;
-        //}
+        if (health <= 70 && health>=31)
+        {
+            Boss3BulletSpawner[] weapons = FindObjectsOfType<Boss3BulletSpawner>();
+            for(int i = 0;i<weapons.Length;i++)
+            {
+                Destroy(weapons[i]);
+            }
+            shotTimer += Time.deltaTime;
+            if (shotTimer > shotTimerMax)
+            {
+                PlayerMovement player = FindObjectOfType<PlayerMovement>();
+
+                Vector2 direction = player.transform.position - transform.position;
+                direction.Normalize();
+                BossBullet proj = Instantiate(projectilePrefab, direction, Quaternion.identity).GetComponent<BossBullet>();
+                Rigidbody2D rigidBodyBullet = proj.GetComponent<Rigidbody2D>();
+                proj.transform.position = new Vector2(transform.position.x,transform.position.y+7.0f);
+                proj.transform.localScale = proj.transform.localScale * 2;
+                rigidBodyBullet.velocity = direction * projectileSpeed * 2;
+                shotTimer = 0;
+            }
+        }
+        if(health<= 30)
+        {
+            transform.position = new Vector2(transform.position.x,transform.position.y-0.001f);
+            if(transform.position.y <= -4)
+            {
+                PlayerMovement player = FindObjectOfType<PlayerMovement>();
+                Destroy(player);
+            }
+        }
 
         healthText.text = "Boss Health: " + health.ToString();
     }
