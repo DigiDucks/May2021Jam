f7ae1fd812d569d8947a62665aeb5ac0a33273c0
TChamp2 <68830714+TChamp2@users.noreply.github.com>
2021-05-19 20:23:35 -0700

    Boss 4 stuff
    Said in title

---
+++ Assets/Scripts/Boss4Controller.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Boss4Controller : MonoBehaviour
+{
+    private Rigidbody2D myRigidbody; // rigid body of the projectile
+    [SerializeField] int health = 100;          // Health of the boss
+    [SerializeField] Text healthText;            // Text of the health
+    float timer = 0.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody2D>();
+        healthText.text = "Boss Health: " + health.ToString();
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+        timer += Time.deltaTime;
+        if(timer< 5.0f)
+        {
+            Renderer render = myRigidbody.GetComponent<Renderer>();
+            render.material.color = Color.red;
+            PlayerMovement player = FindObjectOfType<PlayerMovement>();
+
+            Vector2 direction = player.transform.position - transform.position;
+            direction.Normalize();
+
+            myRigidbody.velocity = direction * 2.0f;
+        }
+        else
+        {
+            myRigidbody.velocity = new Vector2(0, 0);
+            Renderer render = myRigidbody.GetComponent<Renderer>();
+            render.material.color = Color.green;
+
+        }
+        if (timer > 8.0f)
+        {
+            timer = 0;
+            Debug.Log("work");
+        }
+        healthText.text = "Boss Health: " + health.ToString();
+        
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.name.Contains("Sword"))
+            if (collision.gameObject.CompareTag("Sword"))
+            {
+                //if(FindObjectOfType<PlayerSwordAttack>().IsSwordCharged())
+                if (collision.gameObject.GetComponent<PlayerSwordAttack>().IsSwordCharged())
+                {
+                    health -= 3;
+                }
+                else
+                {
+                    health--;
+                }
+            }
+    }
+}
